8b16ddccd637b5e24bd12faf748aca270416f7ed
arnold <zhangcheng17@meituan.com>
2019-06-17 17:29:17 +0800

    [test]modify dirs;

---
+++ src/common/types/ssr.d.ts
@@ -0,0 +1,11 @@
+declare module 'koa-webpack-dev-middleware';
+
+declare module 'koa-webpack-hot-middleware';
+
+declare module 'write-file-webpack-plugin';
+
+declare module 'clean-webpack-plugin';
+
+declare module 'koa-graphql';
+
+declare module 'webpack-deep-scope-plugin';
